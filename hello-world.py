09cc5d537f657707e1e14360d81d5b0ac986db8a
kubsoo <jakub.mazurkiewicz@gmail.com>
2018-07-05 10:41:46 +0200

    edit hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+a = True
+while  a == True:
+	try:
+		a  = int(input("Insert number: "))
+	except ValueError:
+		print ("Nie podales liczby")
+	
+if a == 10:
+	print ("Hello World")
+else:
+	print ("Podales liczbe {} ".format(a))
 
 
-print ("Hello World")
